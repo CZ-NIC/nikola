2b2abbfe1b729c3b3c9c9687494d7ed3cc25cdeb
Stepan Henek <stepan.henek@nic.cz>
2013-05-09 16:40:38 +0200

    Parsing date with datetime when parsing syslog.

--- syslog_parser.py
+++ syslog_parser.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
+from datetime import datetime
 
-def parse_syslog(path):
+def parse_syslog(path, date_len=19, date_format='%Y-%m-%dT%H:%M:%S', **kwargs):
 
-    def parse_line(line):
+    def parse_time_datetime(line, format, **kwargs):
+        time = datetime.strptime(line, format)
+
+        time = time.replace(**kwargs)
+
+        return time.strftime('%Y-%m-%d %H:%M:%S')
+
+    def parse_time_simple(line):
+        return line[:line.find(' ')].replace('T', ' ')
+
+
+    def parse_line(line, date_len, date_format, **kwargs):
         res = {}
         splitted = line.split('iptables: ')
         if len(splitted) != 2:
             return None
-        res[u'time'] = splitted[0][:splitted[0].find(' ')].replace('T', ' ')
+        #res[u'time'] = parse_time_simple(splitted[0])
+        res[u'time'] = parse_time_datetime(line[:date_len], date_format, **kwargs)
         parsed = map(lambda x: x.split('='), splitted[1].split(' '))
         parsed = dict(filter(lambda x: len(x) == 2, parsed))
         res[u'src_port'] = parsed['SPT']
@@ -21,7 +34,7 @@ def parse_syslog(path):
 
     with open(path) as f:
         for line in f:
-            parsed = parse_line(line)
+            parsed = parse_line(line, date_len, date_format, **kwargs)
             if parsed:
                 res.append(parsed)
     
