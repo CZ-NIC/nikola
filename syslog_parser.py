3e1e067b3249ea67b76e067043ec3dc12a437a8e
Stepan Henek <stepan.henek@nic.cz>
2013-07-24 15:08:07 +0200

    parsing: output data modified

--- syslog_parser.py
+++ syslog_parser.py
@@ -13,29 +13,28 @@ def parse_syslog(path, date_len=19, date_format='%Y-%m-%dT%H:%M:%S', **kwargs):
     def parse_time_simple(line):
         return line[:line.find(' ')].replace('T', ' ')
 
-
     def parse_line(line, date_len, date_format, **kwargs):
-        res = {}
-        splitted = line.split('iptables: ')
+        splitted = line.split(': ')
         if len(splitted) != 2:
             return None
-        #res[u'time'] = parse_time_simple(splitted[0])
-        res[u'time'] = parse_time_datetime(line[:date_len], date_format, **kwargs)
+        date = parse_time_datetime(line[:date_len], date_format, **kwargs)
         parsed = map(lambda x: x.split('='), splitted[1].split(' '))
         parsed = dict(filter(lambda x: len(x) == 2, parsed))
-        res[u'src_port'] = parsed['SPT']
-        res[u'dst_port'] = parsed['DPT']
-        res[u'src_addr'] = parsed['SRC']
-        res[u'dst_addr'] = parsed['DST']
-        res[u'protocol'] = parsed['PROTO']
-        return res
+        return date, "%s|%s|%s|%s|%s" % (parsed['SRC'], parsed['SPT'], parsed['DST'], parsed['DPT'], parsed['PROTO'])
 
     res = []
 
     with open(path) as f:
+        last = None
         for line in f:
-            parsed = parse_line(line, date_len, date_format, **kwargs)
-            if parsed:
-                res.append(parsed)
-    
+            parsed_line = parse_line(line, date_len, date_format, **kwargs)
+            if parsed_line:
+                date, parsed = parsed_line
+                # Same packets in the sequence
+                if last and last == parsed:
+                    res[-1][2] += 1
+                else:
+                    res.append([date, parsed, 1])
+                    last = parsed
+
     return res
