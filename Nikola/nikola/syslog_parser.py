7b8c8a85bf6f82dc814e2272d80e3fabae6d83f2
Stepan Henek <stepan.henek@nic.cz>
2013-08-19 12:56:23 +0200

    Parsing fix - when parsing an invalid line just skip, do not exit

--- Nikola/nikola/syslog_parser.py
+++ Nikola/nikola/syslog_parser.py
@@ -1,47 +1,61 @@
 #!/usr/bin/python
 from datetime import datetime
 
-def parse_syslog(path, date_format='%Y-%m-%dT%H:%M:%S', **kwargs):
 
-    def parse_time_datetime(line, format, **kwargs):
+def _parse_time_datetime(line, format, **kwargs):
+    try:
+        # Try to parse
+        time = datetime.strptime(line, format)
+        rest = ''
+    except ValueError, v:
+        # Get the length of unmatched characters
+        unmatched_len = len(v.message.partition('unconverted data remains: ')[2])
         try:
-            time = datetime.strptime(line, format)
-            rest = ''
-        except ValueError, v:
-            unmatched_len = len(v.message.partition('unconverted data remains: ')[2])
             time = datetime.strptime(line[:-unmatched_len], format)
-            rest = line[len(line)-unmatched_len:]
+        except ValueError:
+            # When second parsing return None as date
+            return None, line
 
-        if kwargs:
-            time = time.replace(**kwargs)
+        rest = line[len(line) - unmatched_len:]
 
-        return time.strftime('%Y-%m-%d %H:%M:%S'), rest
+    if kwargs:
+        time = time.replace(**kwargs)
 
-    def parse_line(line, date_format, **kwargs):
+    return time.strftime('%Y-%m-%d %H:%M:%S'), rest
 
-        # Cut the date
-        date, rest = parse_time_datetime(line, date_format, **kwargs)
 
-        # Cut another part up to attributes
-        splitted = rest.split(': ', 1)
-        if len(splitted) != 2:
-            return None
+def _parse_line(line, date_format, **kwargs):
 
-        # Parse the rest
-        parsed = {}
-        for x in splitted[1].split(' '):
-            if '=' in x:
-                key, val = x.split('=', 1)
-                parsed[key] = val
+    # Cut the date
+    date, rest = _parse_time_datetime(line, date_format, **kwargs)
 
-        return date, "%s|%s|%s|%s|%s" % (parsed['SRC'], parsed['SPT'], parsed['DST'], parsed['DPT'], parsed['PROTO'])
+    if not date:
+        return None
+
+    # Cut another part up to attributes
+    splitted = rest.split(': ', 1)
+    if len(splitted) != 2:
+        return None
+
+    # Parse the rest
+    parsed = {}
+    for x in splitted[1].split(' '):
+        if '=' in x:
+            key, val = x.split('=', 1)
+            parsed[key] = val
+
+    return date, "%s|%s|%s|%s|%s" % (parsed['SRC'], parsed['SPT'], parsed['DST'], parsed['DPT'],
+                                     parsed['PROTO'])
+
+
+def parse_syslog(path, date_format='%Y-%m-%dT%H:%M:%S', **kwargs):
 
     res = []
 
     with open(path) as f:
         last = None
         for line in f:
-            parsed_line = parse_line(line, date_format, **kwargs)
+            parsed_line = _parse_line(line, date_format, **kwargs)
             if parsed_line:
                 date, parsed = parsed_line
                 # Same packets in the sequence
