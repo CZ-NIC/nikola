578d2aa66752ad9dbea25903d9893326f1e19f82
Stepan Henek <stepan.henek@nic.cz>
2013-10-03 17:02:01 +0200

    digest replaced by python-libatsha204's get_serial()

--- Nikola/nikola/rpc_wrapper.py
+++ Nikola/nikola/rpc_wrapper.py
@@ -10,18 +10,18 @@ DATE_LEN = 4
 
 
 class WrappedServer(Server):
-    def __init__(self, addr, digest):
-        self.digest = digest
+    def __init__(self, addr, serial=None):
+        self.serial = binascii.hexlify(serial if serial else atsha204.get_serial())
         Server.__init__(self, addr)
 
     def init_session(self):
 
-        res = self.collector.init_session(self.digest)
+        res = self.collector.init_session(self.serial)
         if 'random' in res and len(res['random']) == 2 * RANDOM_LEN \
            and 'timestamp' in res and len(res['timestamp']) == 2 * DATE_LEN:
             random = binascii.unhexlify(res['random'])
             timestamp = res['timestamp']
-            session_key = timestamp + self.digest[:24] \
+            session_key = timestamp + self.serial[:24] \
                 + binascii.hexlify(atsha204.hmac(random))
             self._set_cookie('sessionid="%s"' % session_key)
 
