59c7876dae33468517adc1c505200d255c61cac4
Stepan Henek <stepan.henek@nic.cz>
2015-05-28 10:56:06 +0200

    WAN detection updated (based on ucollect init script)

--- conf/cron/nikola.sh
+++ conf/cron/nikola.sh
@@ -23,26 +23,33 @@
 config_load nikola
 
 get_parameter() {
-    local sec="$1"
-    local var="$2"
-    local val
+	local sec="$1"
+	local var="$2"
+	local val
 
-    config_get val "$sec" "$var"
-    echo "$val"
+	config_get val "$sec" "$var"
+	echo "$val"
 }
 
 get_bool_parameter() {
-    local sec="$1"
-    local var="$2"
-    local val
+	local sec="$1"
+	local var="$2"
+	local val
 
-    config_get_bool val "$sec" "$var" "$3"
-    echo "$val"
+	config_get_bool val "$sec" "$var" "$3"
+	echo "$val"
 }
 
-set_wan() {
-    local cfg="$1"
-    config_get wan "$cfg" ifname
+get_wan() {
+	IFACES="$1"
+	IGNORE="$2"
+	for iface in $IFACES ; do
+		if echo "$IGNORE" | grep -qwF "$iface" ; then
+			continue;
+		fi
+		echo "$iface"
+		return
+	done
 }
 
 server_addr=$(get_parameter server address)
@@ -60,38 +67,47 @@ wan=$(get_parameter main wan_ifname)
 
 
 if [ ! -n "$wan" ]; then
-    # try to get wan using network
-    config_load network
-    wan=$(get_parameter wan ifname)
+	# autodetect using default routes (taken from ucollect init script)
 
-    if [ ! -n "$wan" ]; then
-        # try to get wan using ucollect
-        config_load ucollect
-        config_foreach set_wan interface
-    fi
+	# Look into the routing tables to guess WAN interfaces
+	V4=$(route -n | sed -ne 's/ *$//;/^0\.0\.0\.0  *[0-9.][0-9.]*  *0\.0\.0\.0/s/.* //p')
+	V6=$(route -n -A inet6 | sed -ne 's/ *$//;/^::\/0  /s/.* //p')
+	# Unify them and remove duplicates
+	ALL=$(echo "$V4" "$V6" | sed -e 's/  */ /g;s/ /\n/g' | sort -u)
+	IGNORE=$(uci -X show network | sed -ne 's/^network\.\([^.]*\)=interface$/\1/p' | while read iface ; do
+		proto=$(uci get -q network.$iface.proto)
+		name=$(echo "$proto-$iface" | head -c 15)
+		# TODO: What about L2TP? #3093
+		if [ "$proto" = "6in4" -o "$proto" = "6to4" -o "$proto" = "6rd" -o "$proto" = "dslite" ] ; then
+			# These are tunnels. We can look into them (and do) and they'll travel through the
+			# WAN interface, so we don't need these. Ignore them.
+			echo "$name"
+		fi
+	done)
+	wan=$(get_wan "$ALL" "$IGNORE")
 fi
 
 optional=""
 if [ -n "$max_count" ]; then
-    optional="$optional -m '$max_count'"
+	optional="$optional -m '$max_count'"
 fi
 if [ -n "$log_file" ]; then
-    optional="$optional -l '$log_file'"
+	optional="$optional -l '$log_file'"
 fi
 if [ -n "$date_format" ]; then
-    optional="$optional -f '$date_format'"
+	optional="$optional -f '$date_format'"
 fi
 if [ -n "$log_rotate_conf" ]; then
-    optional="$optional -r '$log_rotate_conf'"
+	optional="$optional -r '$log_rotate_conf'"
 fi
 if [ -n "$wan" ]; then
-    optional="$optional -w $wan"
+	optional="$optional -w $wan"
 fi
 if [ -n "$certificate" ]; then
-    optional="$optional -c $certificate"
+	optional="$optional -c $certificate"
 fi
 if [ "$debug" = 1 ]; then
-    optional="$optional -d"
+	optional="$optional -d"
 fi
 
 eval nikola "$server_addr" "$optional"
